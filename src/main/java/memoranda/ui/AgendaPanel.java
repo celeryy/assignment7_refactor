fccc9a8052fc99224224eb974f1ead26a7a101b4
celeryy <44530826+celeryy@users.noreply.github.com>
2019-04-19 06:25:13 -0700

    Update AgendaPanel.java

--- src/main/java/memoranda/ui/AgendaPanel.java
+++ src/main/java/memoranda/ui/AgendaPanel.java
@@ -50,7 +50,7 @@ public class AgendaPanel extends JPanel {
 	JButton historyForwardB = new JButton();
 	JButton export = new JButton();
 	JEditorPane viewer = new JEditorPane("text/html", "");
-	String[] priorities = {"Muy Alta","Alta","Media","Baja","Muy Baja"};
+	String[] priorities = {"Very High","High","Half","Low","Very Low"};
 	JScrollPane scrollPane = new JScrollPane();
 
 	DailyItemsPanel parentPanel = null;
@@ -78,139 +78,7 @@ public class AgendaPanel extends JPanel {
 		toolBar.setFloatable(false);
 		viewer.setEditable(false);
 		viewer.setOpaque(false);
-		viewer.addHyperlinkListener(new HyperlinkListener() {
-
-			public void hyperlinkUpdate(HyperlinkEvent e) {
-				if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-					String d = e.getDescription();
-					if (d.equalsIgnoreCase("memoranda:events"))
-						parentPanel.alarmB_actionPerformed(null);
-					else if (d.startsWith("memoranda:tasks")) {
-						String id = d.split("#")[1];
-						CurrentProject.set(ProjectManager.getProject(id));
-						parentPanel.taskB_actionPerformed(null);
-					} else if (d.startsWith("memoranda:project")) {
-						String id = d.split("#")[1];
-						CurrentProject.set(ProjectManager.getProject(id));
-					} else if (d.startsWith("memoranda:removesticker")) {
-                        String id = d.split("#")[1];
-                        StickerConfirmation stc = new StickerConfirmation(App.getFrame());
-                        Dimension frmSize = App.getFrame().getSize();
-                        stc.setSize(new Dimension(300,180));
-                        Point loc = App.getFrame().getLocation();
-                        stc.setLocation(
-                                (frmSize.width - stc.getSize().width) / 2 + loc.x,
-                                (frmSize.height - stc.getSize().height) / 2
-                                        + loc.y);
-                        stc.setVisible(true);
-                        if (!stc.CANCELLED) {
-                        EventsManager.removeSticker(id);
-                        CurrentStorage.get().storeEventsManager();}
-                        refresh(CurrentDate.get());
-					} else if (d.startsWith("memoranda:addsticker")) {
-						StickerDialog dlg = new StickerDialog(App.getFrame());
-						Dimension frmSize = App.getFrame().getSize();
-						dlg.setSize(new Dimension(300,380));
-						Point loc = App.getFrame().getLocation();
-						dlg.setLocation(
-								(frmSize.width - dlg.getSize().width) / 2 + loc.x,
-								(frmSize.height - dlg.getSize().height) / 2
-								+ loc.y);
-						dlg.setVisible(true);
-						if (!dlg.CANCELLED) {
-							String txt = dlg.getStickerText();
-							int sP = dlg.getPriority();
-							txt = txt.replaceAll("\\n", "<br>");
-                            txt = "<div style=\"background-color:"+dlg.getStickerColor()+";font-size:"+dlg.getStickerTextSize()+";color:"+dlg.getStickerTextColor()+"; \">"+txt+"</div>";
-							EventsManager.createSticker(txt, sP);
-							CurrentStorage.get().storeEventsManager();
-						}
-						refresh(CurrentDate.get());
-						System.out.println("agregué un sticker");
-					} else if (d.startsWith("memoranda:expandsubtasks")) {
-						String id = d.split("#")[1];
-						gotoTask = id;
-						expandedTasks.add(id);
-						refresh(CurrentDate.get());
-					} else if (d.startsWith("memoranda:closesubtasks")) {
-						String id = d.split("#")[1];
-						gotoTask = id;
-						expandedTasks.remove(id);
-						refresh(CurrentDate.get());
-					} else if (d.startsWith("memoranda:expandsticker")) {
-						String id = d.split("#")[1];
-						Element pre_sticker=(Element)((Map)EventsManager.getStickers()).get(id);
-						String sticker = pre_sticker.getValue();
-						int first=sticker.indexOf(">");
-						int last=sticker.lastIndexOf("<");
-						int backcolor=sticker.indexOf("#");
-						int fontcolor=sticker.indexOf("#", backcolor+1);
-						int sP=Integer.parseInt(pre_sticker.getAttributeValue("priority"));
-						String backGroundColor=sticker.substring(backcolor, sticker.indexOf(';',backcolor));
-						String foreGroundColor=sticker.substring(fontcolor, sticker.indexOf(';',fontcolor));
-						sticker="<html>"+sticker.substring(first+1, last)+"</html>";
-						StickerExpand dlg = new StickerExpand(App.getFrame(),sticker,backGroundColor,foreGroundColor,Local.getString("priority")+": "+Local.getString(priorities[sP]));
-						Dimension frmSize = App.getFrame().getSize();
-						dlg.setSize(new Dimension(300,200));
-						Point loc = App.getFrame().getLocation();
-						dlg.setLocation(
-								(frmSize.width - dlg.getSize().width) / 2 + loc.x,
-								(frmSize.height - dlg.getSize().height) / 2
-								+ loc.y);
-						dlg.stickerText.setText(sticker);
-						dlg.setVisible(true);
-					}else if (d.startsWith("memoranda:editsticker")) {
-						String id = d.split("#")[1];
-						Element pre_sticker=(Element)((Map)EventsManager.getStickers()).get(id);
-						String sticker = pre_sticker.getValue();
-						sticker=sticker.replaceAll("<br>","\n");
-						int first=sticker.indexOf(">");
-						int last=sticker.lastIndexOf("<");
-						int backcolor=sticker.indexOf("#");
-						int fontcolor=sticker.indexOf("#", backcolor+1);
-						int sizeposition=sticker.indexOf("font-size")+10;
-						int size=Integer.parseInt(sticker.substring(sizeposition,sizeposition+2));
-						System.out.println(size+" "+sizeposition);
-						int sP=Integer.parseInt(pre_sticker.getAttributeValue("priority"));
-						String backGroundColor=sticker.substring(backcolor, sticker.indexOf(';',backcolor));
-						String foreGroundColor=sticker.substring(fontcolor, sticker.indexOf(';',fontcolor));
-						StickerDialog dlg = new StickerDialog(App.getFrame(), sticker.substring(first+1, last), backGroundColor, foreGroundColor, sP, size);
-						Dimension frmSize = App.getFrame().getSize();
-						dlg.setSize(new Dimension(300,380));
-						Point loc = App.getFrame().getLocation();
-						dlg.setLocation((frmSize.width - dlg.getSize().width) / 2 + loc.x,
-							 		(frmSize.height - dlg.getSize().height) / 2 + loc.y);
-						dlg.setVisible(true);
-						if (!dlg.CANCELLED) {
-							String txt = dlg.getStickerText();
-							sP = dlg.getPriority();
-							txt = txt.replaceAll("\\n", "<br>");
-							txt = "<div style=\"background-color:"+dlg.getStickerColor()+";font-size:"+dlg.getStickerTextSize()+";color:"+dlg.getStickerTextColor()+";\">"+txt+"</div>";
-							EventsManager.removeSticker(id);
-							EventsManager.createSticker(txt, sP);
-							CurrentStorage.get().storeEventsManager();
-						 }
-						 refresh(CurrentDate.get());
-					}else if (d.startsWith("memoranda:exportstickerst")) {
-						 /*  Falta agregar el exportar sticker mientras tanto..*/
-						 final JFrame parent = new JFrame();
-						 String name = JOptionPane.showInputDialog(parent,Local.getString("Ingrese nombre de archivo a exportar"),null);
-						 new ExportSticker(name).export("txt");
-						 //JOptionPane.showMessageDialog(null,name);
-					}else if (d.startsWith("memoranda:exportstickersh")) {
-						 /*  Falta agregar el exportar sticker mientras tanto..*/
-						 final JFrame parent = new JFrame();
-						 String name = JOptionPane.showInputDialog(parent,Local.getString("Ingrese nombre de archivo a exportar"),null);
-						 new ExportSticker(name).export("html");
-						 //JOptionPane.showMessageDialog(null,name);
-					}else if (d.startsWith("memoranda:importstickers")) {
-						final JFrame parent = new JFrame();
-						String name = JOptionPane.showInputDialog(parent,Local.getString("Ingrese nombre de archivo a importar"),null);
-						new ImportSticker(name).import_file();
-					}
-				}
-			}
-		});
+		
 		historyBackB.setAction(History.historyBackAction);
 		historyBackB.setFocusable(false);
 		historyBackB.setBorderPainted(false);
@@ -290,7 +158,146 @@ public class AgendaPanel extends JPanel {
 		//			(Context.get("SHOW_ACTIVE_TASKS_ONLY") != null)
 		//				&& (Context.get("SHOW_ACTIVE_TASKS_ONLY").equals("true"));
 		//		ppShowActiveOnlyChB.setSelected(isShao);
-		//		toggleShowActiveOnly_actionPerformed(null);		
+		//		toggleShowActiveOnly_actionPerformed(null);	
+		
+		
+	}
+	
+	void jbInit2() throws Exception {	
+		
+		viewer.addHyperlinkListener(new HyperlinkListener() {
+
+			public void hyperlinkUpdate(HyperlinkEvent e) {
+				if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					String d = e.getDescription();
+					if (d.equalsIgnoreCase("memoranda:events"))
+						parentPanel.alarmB_actionPerformed(null);
+					else if (d.startsWith("memoranda:tasks")) {
+						String id = d.split("#")[1];
+						CurrentProject.set(ProjectManager.getProject(id));
+						parentPanel.taskB_actionPerformed(null);
+					} else if (d.startsWith("memoranda:project")) {
+						String id = d.split("#")[1];
+						CurrentProject.set(ProjectManager.getProject(id));
+					} else if (d.startsWith("memoranda:removesticker")) {
+                        String id = d.split("#")[1];
+                        StickerConfirmation stc = new StickerConfirmation(App.getFrame());
+                        Dimension frmSize = App.getFrame().getSize();
+                        stc.setSize(new Dimension(300,180));
+                        Point loc = App.getFrame().getLocation();
+                        stc.setLocation(
+                                (frmSize.width - stc.getSize().width) / 2 + loc.x,
+                                (frmSize.height - stc.getSize().height) / 2
+                                        + loc.y);
+                        stc.setVisible(true);
+                        if (!stc.CANCELLED) {
+                        EventsManager.removeSticker(id);
+                        CurrentStorage.get().storeEventsManager();}
+                        refresh(CurrentDate.get());
+					} else if (d.startsWith("memoranda:addsticker")) {
+						StickerDialog dlg = new StickerDialog(App.getFrame());
+						Dimension frmSize = App.getFrame().getSize();
+						dlg.setSize(new Dimension(300,380));
+						Point loc = App.getFrame().getLocation();
+						dlg.setLocation(
+								(frmSize.width - dlg.getSize().width) / 2 + loc.x,
+								(frmSize.height - dlg.getSize().height) / 2
+								+ loc.y);
+						dlg.setVisible(true);
+						if (!dlg.CANCELLED) {
+							String txt = dlg.getStickerText();
+							int sP = dlg.getPriority();
+							txt = txt.replaceAll("\\n", "<br>");
+                            txt = "<div style=\"background-color:"+dlg.getStickerColor()+";font-size:"+dlg.getStickerTextSize()+";color:"+dlg.getStickerTextColor()+"; \">"+txt+"</div>";
+							EventsManager.createSticker(txt, sP);
+							CurrentStorage.get().storeEventsManager();
+						}
+						refresh(CurrentDate.get());
+						System.out.println("Add a sticker");
+					} else if (d.startsWith("memoranda:expandsubtasks")) {
+						String id = d.split("#")[1];
+						gotoTask = id;
+						expandedTasks.add(id);
+						refresh(CurrentDate.get());
+					} else if (d.startsWith("memoranda:closesubtasks")) {
+						String id = d.split("#")[1];
+						gotoTask = id;
+						expandedTasks.remove(id);
+						refresh(CurrentDate.get());
+					} else if (d.startsWith("memoranda:expandsticker")) {
+						String id = d.split("#")[1];
+						Element pre_sticker=(Element)((Map)EventsManager.getStickers()).get(id);
+						String sticker = pre_sticker.getValue();
+						int first=sticker.indexOf(">");
+						int last=sticker.lastIndexOf("<");
+						int backcolor=sticker.indexOf("#");
+						int fontcolor=sticker.indexOf("#", backcolor+1);
+						int sP=Integer.parseInt(pre_sticker.getAttributeValue("priority"));
+						String backGroundColor=sticker.substring(backcolor, sticker.indexOf(';',backcolor));
+						String foreGroundColor=sticker.substring(fontcolor, sticker.indexOf(';',fontcolor));
+						sticker="<html>"+sticker.substring(first+1, last)+"</html>";
+						StickerExpand dlg = new StickerExpand(App.getFrame(),sticker,backGroundColor,foreGroundColor,Local.getString("priority")+": "+Local.getString(priorities[sP]));
+						Dimension frmSize = App.getFrame().getSize();
+						dlg.setSize(new Dimension(300,200));
+						Point loc = App.getFrame().getLocation();
+						dlg.setLocation(
+								(frmSize.width - dlg.getSize().width) / 2 + loc.x,
+								(frmSize.height - dlg.getSize().height) / 2
+								+ loc.y);
+						dlg.stickerText.setText(sticker);
+						dlg.setVisible(true);
+					}else if (d.startsWith("memoranda:editsticker")) {
+						String id = d.split("#")[1];
+						Element pre_sticker=(Element)((Map)EventsManager.getStickers()).get(id);
+						String sticker = pre_sticker.getValue();
+						sticker=sticker.replaceAll("<br>","\n");
+						int first=sticker.indexOf(">");
+						int last=sticker.lastIndexOf("<");
+						int backcolor=sticker.indexOf("#");
+						int fontcolor=sticker.indexOf("#", backcolor+1);
+						int sizeposition=sticker.indexOf("font-size")+10;
+						int size=Integer.parseInt(sticker.substring(sizeposition,sizeposition+2));
+						System.out.println(size+" "+sizeposition);
+						int sP=Integer.parseInt(pre_sticker.getAttributeValue("priority"));
+						String backGroundColor=sticker.substring(backcolor, sticker.indexOf(';',backcolor));
+						String foreGroundColor=sticker.substring(fontcolor, sticker.indexOf(';',fontcolor));
+						StickerDialog dlg = new StickerDialog(App.getFrame(), sticker.substring(first+1, last), backGroundColor, foreGroundColor, sP, size);
+						Dimension frmSize = App.getFrame().getSize();
+						dlg.setSize(new Dimension(300,380));
+						Point loc = App.getFrame().getLocation();
+						dlg.setLocation((frmSize.width - dlg.getSize().width) / 2 + loc.x,
+							 		(frmSize.height - dlg.getSize().height) / 2 + loc.y);
+						dlg.setVisible(true);
+						if (!dlg.CANCELLED) {
+							String txt = dlg.getStickerText();
+							sP = dlg.getPriority();
+							txt = txt.replaceAll("\\n", "<br>");
+							txt = "<div style=\"background-color:"+dlg.getStickerColor()+";font-size:"+dlg.getStickerTextSize()+";color:"+dlg.getStickerTextColor()+";\">"+txt+"</div>";
+							EventsManager.removeSticker(id);
+							EventsManager.createSticker(txt, sP);
+							CurrentStorage.get().storeEventsManager();
+						 }
+						 refresh(CurrentDate.get());
+					}else if (d.startsWith("memoranda:exportstickerst")) {
+						 /*  Falta agregar el exportar sticker mientras tanto..*/
+						 final JFrame parent = new JFrame();
+						 String name = JOptionPane.showInputDialog(parent,Local.getString("Enter file name to export"),null);
+						 new ExportSticker(name).export("txt");
+						 //JOptionPane.showMessageDialog(null,name);
+					}else if (d.startsWith("memoranda:exportstickersh")) {
+						 /*  Falta agregar el exportar sticker mientras tanto..*/
+						 final JFrame parent = new JFrame();
+						 String name = JOptionPane.showInputDialog(parent,Local.getString("Enter file name to export"),null);
+						 new ExportSticker(name).export("html");
+						 //JOptionPane.showMessageDialog(null,name);
+					}else if (d.startsWith("memoranda:importstickers")) {
+						final JFrame parent = new JFrame();
+						String name = JOptionPane.showInputDialog(parent,Local.getString("Enter file name to import"),null);
+						new ImportSticker(name).import_file();
+					}
+				}
+			}
+		});
 	}
 
 	public void refresh(CalendarDate date) {
